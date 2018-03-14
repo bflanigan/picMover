76536ec7b1b5125807db045e160eaede4a636fe4
Brian Flanigan <brian_flanigan@cable.comcast.com>
2018-03-14 12:36:54 -0800

    latest

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -13,12 +14,87 @@ import (
 	"github.com/rwcarlsen/goexif/exif"
 )
 
+var (
+	startDir, destDir, prevDir string
+)
+
 func main() {
 
-	var startDir, destDir string
+	var analyze bool
+	var checkFile string
+
 	flag.StringVar(&startDir, "startDir", "", "Start traversing from here")
 	flag.StringVar(&destDir, "destDir", "", "Put renamed files here")
+	flag.BoolVar(&analyze, "analyze", false, "Check for metadata on file")
+	flag.StringVar(&checkFile, "checkfile", "", "File to examine")
 	flag.Parse()
+
+	prevDir = "/media/camera"
+
+	if analyze {
+		if len(checkFile) == 0 {
+			log.Fatalf("You did not specify a file to examine")
+		}
+
+		f, err := os.Open(checkFile)
+		if err != nil {
+			log.Fatalf("Failed to open %s for reading due to error: %v", checkFile, err)
+		}
+		defer f.Close()
+
+		x, err := exif.Decode(f)
+		if err != nil {
+			log.Fatalf("Failed to decode exif tag due to error: %v", err)
+		}
+
+		dateTaken, err := x.DateTime()
+		if err != nil {
+			log.Fatalf("Parsed exif tag but was unable to extract timestamp due to error: %v", err)
+		}
+
+		var strday, strhour, strmin, strsec string
+
+		year, month, day := dateTaken.Date()
+		hour, min, sec := dateTaken.Clock()
+
+		stryear := strconv.Itoa(year)
+		strmonth := numMonth(month)
+
+		if day < 10 {
+			strday = strconv.Itoa(day)
+			strday = "0" + strday
+		} else {
+			strday = strconv.Itoa(day)
+		}
+
+		if hour < 10 {
+			strhour = strconv.Itoa(hour)
+			strhour = "0" + strhour
+		} else {
+			strhour = strconv.Itoa(hour)
+		}
+
+		if min < 10 {
+			strmin = strconv.Itoa(min)
+			strmin = "0" + strmin
+		} else {
+			strmin = strconv.Itoa(min)
+		}
+
+		if sec < 10 {
+			strsec = strconv.Itoa(sec)
+			strsec = "0" + strsec
+		} else {
+			strsec = strconv.Itoa(sec)
+		}
+
+		//origFilename := filepath.Base(checkFile)
+		//newFilename := fmt.Sprintf("%s%s%s_%s%s%s_%s", stryear, strmonth, strday, strhour, strmin, strsec, origFilename)
+
+		fmt.Printf("%s - %s%s%s_%s%s%s\n", checkFile, stryear, strmonth, strday, strhour, strmin, strsec)
+		os.Exit(1)
+
+	}
 
 	if len(startDir) == 0 || len(destDir) == 0 {
 		log.Fatalf("You did not specify either a starting or destination dir")
@@ -34,7 +110,7 @@ func main() {
 func walkFunc(path string, info os.FileInfo, err error) error {
 
 	if err != nil {
-		log.Printf("Got error entering walkFunc for file %s, error: %v\n", path, err)
+		log.Printf("Got error entering walkFunc for file %s - skipping, error: %v", path, err)
 		return nil
 	}
 
@@ -43,17 +119,19 @@ func walkFunc(path string, info os.FileInfo, err error) error {
 		return nil
 	}
 
+	// copy AAE files
 	if info.Size() < 8192 {
-		log.Printf("Skipping small file: %s with size: %d", path, info.Size())
+		copyInvalidExtension(path)
 		return nil
 	}
 
 	fields := strings.Split(info.Name(), `.`)
-	lastField := len(fields) - 1
-	extension := fields[lastField]
+	extension := fields[len(fields)-1]
 	lowerExtension := strings.ToLower(extension)
 
+	// get PNG, MOV, MP4
 	if lowerExtension != "jpg" {
+		copyInvalidExtension(path)
 		return nil
 	}
 
@@ -65,30 +143,30 @@ func walkFunc(path string, info os.FileInfo, err error) error {
 
 	x, err := exif.Decode(f)
 	if err != nil {
-		log.Printf("Failed to decode exif from file: %s due to error: %v", path, err)
+		copyMissingEXIF(f)
+		return nil
 	}
 
 	dateTaken, err := x.DateTime()
 	if err != nil {
-		log.Printf("Failed to extract Date + Time pic was taken on file: %s due to error: %v", path, err)
+		copyMissingEXIF(f)
 		return nil
 	}
 
+	var strday, strhour, strmin, strsec string
+
 	year, month, day := dateTaken.Date()
+	hour, min, sec := dateTaken.Clock()
 
 	stryear := strconv.Itoa(year)
 	strmonth := numMonth(month)
 
-	var strday string
 	if day < 10 {
 		strday = strconv.Itoa(day)
 		strday = "0" + strday
 	} else {
 		strday = strconv.Itoa(day)
 	}
-
-	hour, min, sec := dateTaken.Clock()
-	var strhour, strmin, strsec string
 
 	if hour < 10 {
 		strhour = strconv.Itoa(hour)
@@ -112,10 +190,37 @@ func walkFunc(path string, info os.FileInfo, err error) error {
 	}
 
 	origFilename := filepath.Base(path)
+	// remove whitespace in previous files and replace with _
+	origFilename = strings.Replace(origFilename, " ", "_", -1)
+	newFilename := fmt.Sprintf("%s%s%s_%s%s%s_%s", stryear, strmonth, strday, strhour, strmin, strsec, origFilename)
 
-	stamp := fmt.Sprintf("%s%s%s_%s%s%s_%s", stryear, strmonth, strday, strhour, strmin, strsec, origFilename)
+	destpath := fmt.Sprintf("%s/%s-%s", destDir, stryear, strmonth)
+	prevDir = destpath
 
-	log.Printf("File: %s NewName: %v, ExtractedData: %s", path, stamp, dateTaken.String())
+	// create destination dir
+	_, err = os.Stat(destpath)
+	if err != nil {
+		err = os.MkdirAll(destpath, 0755)
+		if err != nil {
+			log.Fatalf("Failed to make destination dir: %s due to error: %v", destpath, err)
+		}
+	}
+
+	destFile := fmt.Sprintf("%s/%s", destpath, newFilename)
+	df, err := os.Create(destFile)
+	if err != nil {
+		log.Fatalf("Failed to create destination file: %v", err)
+	}
+	defer df.Close()
+
+	// seek back to the beginning for kicks in case we are not at offset 0
+	f.Seek(0, 0)
+	written, err := io.Copy(df, f)
+	if err != nil {
+		log.Fatalf("Failed to copy to destination file %s due to error: %v", df.Name(), err)
+	}
+
+	log.Printf("Copied %d bytes %s to %s", written, f.Name(), df.Name())
 	return nil
 }
 
@@ -148,4 +253,52 @@ func numMonth(month time.Month) string {
 	}
 
 	return ""
+}
+
+// copyMissingEXIF is called to copy .JPG files into the last known good dir
+func copyMissingEXIF(sf *os.File) {
+
+	sf.Seek(0, 0)
+
+	sourceFilename := filepath.Base(sf.Name())
+
+	df, err := os.Create(prevDir + "/" + sourceFilename)
+	if err != nil {
+		log.Fatalf("Failed to create destination file: %s due to error: %v", df.Name(), err)
+	}
+	defer df.Close()
+
+	written, err := io.Copy(df, sf)
+	if err != nil {
+		log.Fatalf("Failed to copy to destination file %s due to error: %v", df.Name(), err)
+	}
+
+	log.Printf("Copied %d bytes %s to %s", written, sf.Name(), df.Name())
+
+}
+
+// copyInvalidExtension copies to prevDir all non JPG files
+func copyInvalidExtension(sourcefile string) {
+
+	sourceFilename := filepath.Base(sourcefile)
+
+	df, err := os.Create(prevDir + "/" + sourceFilename)
+	if err != nil {
+		log.Fatalf("Failed to create destination file: %s due to error: %v", df.Name(), err)
+	}
+	defer df.Close()
+
+	sf, err := os.Open(sourcefile)
+	if err != nil {
+		log.Fatalf("Failed to open sourcefile: %s due to error: %v", sourcefile, err)
+	}
+	defer sf.Close()
+
+	written, err := io.Copy(df, sf)
+	if err != nil {
+		log.Fatalf("Failed to copy to destination file %s due to error: %v", df.Name(), err)
+	}
+
+	log.Printf("Copied %d bytes %s to %s", written, sf.Name(), df.Name())
+
 }
